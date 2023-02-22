7c5f75d820224d999fe2278962837483ef963360
Ecem <ekavazka27@alumnes.ub.edu>
2023-02-22 20:21:51 +0100

    settings

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # start your application
-python manage.py runserver 0.0.0.0:$PORT
+python manage.py runserver 0.0.0.0:8080
 python manage.py makemigrations DataVisualization
 python manage.py migrate
